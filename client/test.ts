d399e498206b07ed6f919dbf295d0d36060e1e39
Ben Findley <ben.g.findley@gmail.com>
2020-12-11 10:36:07 -0800

    display items on page

--- client/test.ts
+++ client/test.ts
@@ -1,18 +1,25 @@
 var inputBox = document.getElementById("add-item-textbox") as HTMLInputElement;
+var itemList = document.getElementById("items") as HTMLDivElement;
 
 const newItem = () => {
     fetch('/api/', {
         method: 'POST', 
-        mode: 'cors',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify({"text":inputBox.value})
     })
         .then(currentItems => {
-            console.log(currentItems);
             return currentItems.json();
         })
-        .then(currentItems => console.log(currentItems.value))
+        .then(currentItems => {
+            itemList.innerHTML = "";
+            currentItems.value.forEach((item: string) => {
+                var entry = document.createElement('p');
+                entry.className = 'item-entry';
+                entry.innerText = item;
+                itemList.appendChild(entry);
+            });
+        })
         .then(() => inputBox.value = "");
 }
