0d77ff3cf8a8ba279edac76b9c9489893688bd88
Ben Findley <ben.g.findley@gmail.com>
2020-12-16 15:34:54 -0800

    close button

--- client/test.ts
+++ client/test.ts
@@ -2,17 +2,21 @@ import prettyMilliseconds from 'pretty-ms';
 
 var inputMemo = document.getElementById("memo") as HTMLInputElement;
 var inputEventDate = document.getElementById("event") as HTMLInputElement;
-var itemList = document.getElementById("items") as HTMLDivElement;
+
 var composeButton = document.getElementById("compose-button") as HTMLButtonElement;
 var composeOverlay = document.getElementById("compose-overlay") as HTMLDivElement;
 var composeForm = document.getElementById("compose-form") as HTMLFormElement;
 
+var itemList = document.getElementById("items") as HTMLDivElement;
+
 class RawItem {
+    id: string;
     memo: string;
     created_time: string;
     event_time: string;
 
-    constructor(nmemo: string, ncreated_time: string, nevent_time: string) {
+    constructor(nid: string, nmemo: string, ncreated_time: string, nevent_time: string) {
+        this.id = nid;
         this.memo = nmemo;
         this.created_time = ncreated_time;
         this.event_time = nevent_time;
@@ -20,11 +24,13 @@ class RawItem {
 }
 
 class Item {
+    id: string;
     memo: string;
     created_time: Date;
     event_time: Date;
 
     constructor(raw: RawItem) {
+        this.id = raw.id;
         this.memo = raw.memo;
         this.created_time = new Date(raw.created_time);
         this.event_time = new Date(raw.event_time);
@@ -34,7 +40,7 @@ class Item {
 var data: Array<Item>;
 
 const createEntryFromItem = (item: Item) => {
-    let time = item.event_time.getTime() - Date.now();
+    let time = item.event_time.getTime() - Date.now() + item.event_time.getTimezoneOffset() * 60 * 1000;
     let humanReadableTime: string;
     if(time < 0) {
         humanReadableTime = prettyMilliseconds(-time, {compact: true}) + " ago";
@@ -42,11 +48,43 @@ const createEntryFromItem = (item: Item) => {
         humanReadableTime = "in " + prettyMilliseconds(time, {compact: true});
     }
 
-    let entry: HTMLElement = document.createElement('p');
-    entry.className = 'item-entry';
-    entry.innerText = item.memo + " " + humanReadableTime;
+    let entryBox: HTMLElement = document.createElement('p');
+    entryBox.className = 'item-entry-box';
 
-    return entry;
+    let entryTime: HTMLDivElement = document.createElement('div');
+    entryTime.className = 'item-entry-time';
+    entryTime.innerText = humanReadableTime;
+
+    let entryMemo: HTMLDivElement = document.createElement('div');
+    entryMemo.className = 'item-entry-memo';
+    entryMemo.innerText = item.memo;
+
+    let entryDeleteButton: HTMLButtonElement = document.createElement('button');
+    entryDeleteButton.className = 'item-entry-delete-button';
+    entryDeleteButton.innerText = '✖';
+    entryDeleteButton.onclick = () => {
+        fetch(`/api/${item.id}`, {method: 'DELETE'})
+            .then(refreshItems);
+    };
+
+    entryBox.appendChild(entryTime);
+    entryBox.appendChild(entryMemo);
+    entryBox.appendChild(entryDeleteButton);
+
+    return entryBox;
+}
+
+const refreshItems = () => {
+    fetch('/api/', {method: 'GET'})
+        .then(currentItems => currentItems.json())
+        .then(currentItems => {
+            data = currentItems.value.map((raw: RawItem) => new Item(raw));
+            itemList.innerHTML = "";
+            data.forEach(item => {
+                itemList.appendChild(createEntryFromItem(item));
+            });
+            console.log(data);
+        })
 }
 
 composeButton.onclick = () => {
@@ -62,16 +100,7 @@ composeForm.onsubmit = () => {
         },
         body: JSON.stringify({"memo":inputMemo.value, "event_time":date.toJSON()})
     })
-        .then(() => fetch('/api/', {method: 'GET'}))
-        .then(currentItems => currentItems.json())
-        .then(currentItems => {
-            data = currentItems.value.map((raw: RawItem) => new Item(raw));
-            itemList.innerHTML = "";
-            data.forEach(item => {
-                itemList.appendChild(createEntryFromItem(item));
-            });
-            console.log(data);
-        })
+        .then(refreshItems)
         .then(() => {
             inputMemo.value = "";
             inputEventDate.value = "";
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::convert::Infallible;
+use std::hash::{Hash, Hasher};
 
 use warp::Filter;
 use chrono::prelude::*;
@@ -7,12 +8,13 @@ type Data = std::sync::Arc<std::sync::Mutex<Vec<Datum>>>;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 struct Datum {
+    id: String,
     memo: String,
     created_time: DateTime<Utc>,
     event_time: DateTime<Utc>,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, Hash)]
 struct NewItem {
     memo: String,
     event_time: String,
@@ -45,12 +47,21 @@ fn main() {
         // -> {value: [Data]}
         let get_items = warp::path!("api")
             .and(warp::get())
-            .and(data_as_filter(db))
+            .and(data_as_filter(db.clone()))
             .and_then(get_data);
+
+        // DELETE
+        // /api/:id
+        // -> 204 No content | 404 Not found
+        let remove_item = warp::path!("api" / String)
+            .and(warp::delete())
+            .and(data_as_filter(db))
+            .and_then(remove_item_from_data);
 
         let app = site
             .or(post_item)
-            .or(get_items);
+            .or(get_items)
+            .or(remove_item);
 
         warp::serve(app).run(([127, 0, 0, 1], 3030)).await;
     });
@@ -66,7 +77,7 @@ async fn add_item_to_data(
     rdata: Data,
     item: NewItem,
 ) -> Result<impl warp::Reply, Infallible> {
-    let datum = Datum::new(item.memo, &item.event_time);
+    let datum = Datum::new(item);
     if let Err(e) = &datum {
         if e.is::<chrono::ParseError>() {
             println!("bad request made: {:?}", e);
@@ -81,6 +92,25 @@ async fn add_item_to_data(
     Ok(warp::http::StatusCode::CREATED)
 }
 
+async fn remove_item_from_data(
+    id: String,
+    rdata: Data,
+) -> Result<impl warp::Reply, Infallible> {
+    let mut data = rdata.lock().unwrap();
+
+    let possible_item_index = data.iter()
+        .enumerate()
+        .find(|(_, datum)| datum.id == id)
+        .map(|(i, _)| i);
+
+    if let Some(i) = possible_item_index {
+        data.remove(i);
+        Ok(warp::http::StatusCode::NO_CONTENT)
+    } else {
+        Ok(warp::http::StatusCode::NOT_FOUND)
+    }
+}
+
 async fn get_data(
     rdata: Data,
 ) -> Result<impl warp::Reply, Infallible> {
@@ -92,11 +122,15 @@ async fn get_data(
 }
 
 impl Datum {
-    fn new(nmemo: String, nevent_time: &str) -> anyhow::Result<Datum> {
+    fn new(nitem: NewItem) -> anyhow::Result<Datum> {
+        let mut hasher = std::collections::hash_map::DefaultHasher::new();
+        nitem.hash(&mut hasher);
         Ok(Datum {
-            memo: nmemo,
+            id: format!("{:x}", hasher.finish()),
+            memo: nitem.memo,
             created_time: Utc::now(),
-            event_time: nevent_time.parse()?,
+            event_time: nitem.event_time.parse()?,
         })
     }
 }
+
